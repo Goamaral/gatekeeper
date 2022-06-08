e15c5b8f710b92259723b2d471a504d53560297b
Gonçalo Amaral <goncalo@maven.pet>
2022-06-09 00:14:20 +0100

    Example client - Skip connect if already connected

--- example/public/index.js
+++ example/public/index.js
@@ -1,25 +1,47 @@
-window.onload = () => {
+window.onload = async () => {
   const connectButtonEl = document.getElementById("connect_button")
   const loginButtonEl = document.getElementById("login_button")
-  const getAuthUserButtonEl = document.getElementById("get_auth_user")
   const errorEl = document.getElementById("error")
   const authUserEl = document.getElementById("auth_user")
 
+  connectButtonEl.style.display = "none"
+  loginButtonEl.style.display = "none"
+
+  let user = null
+  let signer, walletAddress
+  
+  const fetchAuthUser = async (showError = true) => {
+    try {
+      user = (await axios.get("/auth/user")).data.user
+    } catch (err) {
+      if (showError) errorEl.innerText = `${err.response.statusText}: ${err.response.data.error}`
+    }
+  }
+  
   if (!window.ethereum) {
     window.alert("Metamask not installed")
   }
-
+  
   const provider = new ethers.providers.Web3Provider(window.ethereum)
 
-  // TODO: Skip connect if already connected
-  console.log(provider.listAccounts())
-  loginButtonEl.style.display = "none"
+  const alreadyConnected = (await provider.listAccounts()).length != 0
+  if (!alreadyConnected) {
+    connectButtonEl.style.display = "inline-block"
+  } else {
+    signer = provider.getSigner()
+    walletAddress = await signer.getAddress()
+    await fetchAuthUser(false)
 
-  let signer, walletAddress
+    if (!user) {
+      loginButtonEl.style.display = "inline-block"
+    } else {
+      authUserEl.innerText = JSON.stringify(user)
+    }
+  }
 
   // Connect to wallet
   connectButtonEl.onclick = async () => {
-    provider.send("eth_requestAccounts", [])
+    await provider.send("eth_requestAccounts", [])
     signer = provider.getSigner()
     walletAddress = await signer.getAddress()
 
@@ -34,20 +56,13 @@ window.onload = () => {
       const signedChallenge = await signer.signMessage(challenge)
       await axios.post("/auth/login", { wallet_address: walletAddress, signed_challenge: signedChallenge })
     } catch (err) {
+      console.error(err)
       errorEl.innerText = `${err.response.statusText}: ${err.response.data.error}`
     }
-
 
     loginButtonEl.style.display = "none"
-  }
 
-  // Get authenticated user
-  getAuthUserButtonEl.onclick = async () => {
-    try {
-      const { user } = (await axios.get("/auth/user")).data
-      authUserEl.innerText = JSON.stringify(user)
-    } catch (err) {
-      errorEl.innerText = `${err.response.statusText}: ${err.response.data.error}`
-    }
+    await fetchAuthUser()
+    if (user) authUserEl.innerText = JSON.stringify(user)
   }
 }
